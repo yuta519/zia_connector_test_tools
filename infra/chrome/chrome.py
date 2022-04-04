0fce4df064cf2bc08b14fce850e0f3d5b144f7e0
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-04-05 07:07:18 +0900

    detect os and screenshot

--- infra/chrome/chrome.py
+++ infra/chrome/chrome.py
@@ -1,12 +1,26 @@
+import os
+import platform
 import time
 
 from selenium import webdriver
 
-chrome_driver_path = "infra/chrome/driver/mac/chromedriver"
+# chrome_driver_path = "infra/chrome/driver/mac/chromedriver"
+def _detect_os():
+    os = platform.system()
+    if os == "Windows":
+        return "infra/chrome/driver/win/chromedriver"
+    elif os == "Darwin":
+        return "infra/chrome/driver/mac/chromedriver"
+    elif os == "Linux":
+        return "infra/chrome/driver/linux/chromedriver"
+
+
+chrome_driver_path = _detect_os()
 
 
 def get(url: str) -> None:
     driver = webdriver.Chrome(executable_path=chrome_driver_path)
     driver.get(url)
+    driver.save_screenshot("logs/screenshot/screen.png")
     time.sleep(1)
     driver.close()
