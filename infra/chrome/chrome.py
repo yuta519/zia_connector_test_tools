9b29ee9f144bacb76eb7858425e9083474ef1ddc
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-04-07 08:04:26 +0900

    seperate function responsibilities

--- infra/chrome/chrome.py
+++ infra/chrome/chrome.py
@@ -1,11 +1,10 @@
-import os
 import platform
 import time
 
 from selenium import webdriver
 
-# chrome_driver_path = "infra/chrome/driver/mac/chromedriver"
-def _detect_os():
+
+def _decide_chromedriver_by_os():
     os = platform.system()
     if os == "Windows":
         return "infra/chrome/driver/win/chromedriver"
@@ -15,12 +14,22 @@ def _detect_os():
         return "infra/chrome/driver/linux/chromedriver"
 
 
-chrome_driver_path = _detect_os()
+chrome_driver_path = _decide_chromedriver_by_os()
 
 
-def get(url: str) -> None:
-    driver = webdriver.Chrome(executable_path=chrome_driver_path)
-    driver.get(url)
-    driver.save_screenshot("logs/screenshot/screen.png")
-    time.sleep(1)
+def initialize_driver() -> webdriver:
+    return webdriver.Chrome(executable_path=chrome_driver_path)
+
+
+def destroy_driver(driver: webdriver) -> None:
     driver.close()
+
+
+def get(driver: webdriver, url: str) -> None:
+    driver.get(url)
+    # driver.save_screenshot("logs/screenshot/screen.png")
+    time.sleep(1)
+
+
+def take_screenshot(driver: webdriver) -> None:
+    driver.save_screenshot("logs/screenshot/screen.png")
