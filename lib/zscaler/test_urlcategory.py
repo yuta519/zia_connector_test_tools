4c4ffa94ac37b6babe28a858ac916e92b9f2ff1a
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-04-07 10:26:20 +0900

    add detection of allow or blocked to logfilew

--- lib/zscaler/test_urlcategory.py
+++ lib/zscaler/test_urlcategory.py
@@ -1,3 +1,5 @@
+import logging
+
 from infra.chrome import chrome
 from utils.zscaler.constants import BLOCK_TITLE
 from utils.zscaler.constants import URLCATEGORY_URLS
@@ -9,8 +11,11 @@ def access_allow_url(urlcategory: str):
             driver = chrome.initialize_driver()
             chrome.get(driver, "https://" + url)
             chrome.take_screenshot(driver)
-            if chrome.fetch_page_title(driver) != BLOCK_TITLE:
-                print("allowed")
+            logging.info(
+                f"{url}[{urlcategory}] is correctly allowed"
+            ) if chrome.fetch_page_title(driver) != BLOCK_TITLE else logging.warn(
+                f"{url}[{urlcategory}] is unexpectedly blocked"
+            )
         except:
             print("???")
 
--- utils/common/prepare.py
+++ utils/common/prepare.py
@@ -4,17 +4,22 @@ import os
 LOG_DIR = "logs"
 SCREENSHOT_DIR = "logs/screenshot"
 
-# TODO@yuta519 Add log time
+
+# TODO@yuta519 create logfile when it is not created
 def _make_directory(DIR: str) -> None:
     if not os.path.exists(DIR):
         os.makedirs(DIR)
         logging.info(f"Made {DIR} directory")
 
-    logging.basicConfig(filename="logs/running.log", level=logging.DEBUG)
-
-    logging.info(f"{DIR} exits.") if os.path.isdir(DIR) else logging.warn(
-        f"{DIR} does not exits."
+    logging.basicConfig(
+        filename="logs/running.log",
+        format="%(asctime)-15s %(message)s",
+        level=logging.INFO,
     )
+
+    logging.info(f"{DIR} diretory already exits.") if os.path.isdir(
+        DIR
+    ) else logging.warn(f"{DIR} does not exits.")
 
 
 # TODO@yuta519 Add function to run by external files
